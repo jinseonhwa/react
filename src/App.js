a254d297cb94c95f900d66c707c4023c7ad6e0ea
jinseonhwa <dordam0810@gmail.com>
2022-11-20 15:17:08 +0900

    study react Context API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from "react";
+// import ColorBox from "./components/ColorBox";
+// import ColorContext from "./contexts/color";
+import ColorBoxDynamic from "./components/ColorBoxDynamic";
+import { ColorProvider } from "./contexts/colorDynamic";
+import SelectColors from "./components/SelectColors";
 
 function App() {
   return (
-    <>
-    </>
+    // Provider는 value 값 필수
+    // <ColorContext.Provider value={{color: 'red'}}>
+    //    <>
+    //     <ColorBox/>
+    //   </>
+    // </ColorContext.Provider>
+    <ColorProvider>
+      <>
+        <SelectColors/>
+        <br/>
+        <br/>
+        <ColorBoxDynamic/>
+      </>
+    </ColorProvider>
   );
 }
 
---
+++ src/components/ColorBoxDynamic.js
@@ -0,0 +1,42 @@
+import React from 'react';
+// import { ColorConsumer } from '../contexts/colorDynamic';
+import { useContext } from 'react';
+import ColorContext from '../contexts/colorDynamic';
+
+
+const ColorBoxDynamic = () => {
+  const { state } = useContext(ColorContext);
+
+  return (
+    // <ColorConsumer>
+    //   {({state}) => (
+    //     <>
+    //       <div style={{
+    //         width: '64px',
+    //         height: '64px',
+    //         background: state.color
+    //       }}/>
+    //       <div style={{
+    //         width: '32px',
+    //         height: '32px',
+    //         background: state.subcolor
+    //       }}/>
+    //     </>
+    //   )}
+    // </ColorConsumer>
+    <>
+      <div style={{
+        width: '64px',
+        height: '64px',
+        background: state.color
+      }}/>
+      <div style={{
+        width: '32px',
+        height: '32px',
+        background: state.subcolor
+      }}/>
+    </>
+  );
+};
+
+export default ColorBoxDynamic;
